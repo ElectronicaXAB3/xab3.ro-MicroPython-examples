08c69d2780e77c84a62c12520db80fd8ed9bc5a5
popalexr <alexpop8@hotmail.com>
2023-07-06 09:50:16 +0300

    wib

---
+++ [ESP32] Servo-motor 9G - WiFi/Exemplu/main.py
@@ -0,0 +1,54 @@
+import network
+import urequests
+import BlynkLib as blynklib
+from machine import Pin
+from servo import Servo
+
+
+# configurari WiFi
+wifi_ssid = "Rasty_Home_IPH"
+wifi_password = "hex12300"
+
+if len(wifi_ssid) == 0 or len(wifi_password) == 0: #verificare daca datele pentru wifi sunt completate
+    print("!!! wifi_ssid sau wifi_password nu trebuie sa fie goale.")
+    while True:
+        pass
+
+#configurare platforma Blynk
+blynk_token = 'xpph8UwjDK9ArQ2YkubAOsKr1xREsXCY'
+if len(blynk_token) == 0: #verificare daca datele pentru wifi sunt completate
+    print("!!! blynk_token nu trebuie sa fie gol.")
+    while True:
+        pass
+
+blynk_pin_servo = 1 #virtual pin 1
+servo_pin = 22 #gpio 22
+
+wifi = network.WLAN(network.STA_IF) #initializare wifi
+wifi.active(True)
+wifi.connect(wifi_ssid, wifi_password) #conectare wifi
+
+print("Se incearca conectarea la WiFi. Asigura-te ca ai introdus datele corect.")
+while not wifi.isconnected(): #Incercare de conectare la wifi
+    pass
+
+print("Conectare reusita.\nIP: ", wifi.ifconfig()) #Afisare mesaj si IP, in cazul in care conexiunea a reusit
+
+blynk = blynklib.Blynk(blynk_token, insecure=True) #initializare blynk
+
+servo = Servo(pin = servo_pin) #initializare servo la pinul GPIO 22
+
+@blynk.on("V" + str(blynk_pin_servo)) #event pentru citirea valorilor de pe platforma, Virtual Pin 1
+def servo_read(value):
+    #value[0] - numarul citit
+    servo.move(int(value[0])) #seteaza unghiul servo-ului la valoarea citita
+    
+
+@blynk.on("connected") #event pentru conexiunea la platforma
+def blynk_connected(ping):
+    print('Blynk ready. Ping:', ping, 'ms')
+    blynk.sync_virtual(1)
+
+while True:
+    blynk.run()
+
---
+++ [ESP32] Servo-motor 9G - WiFi/README.md
@@ -0,0 +1,26 @@
+# Servo-motor 9G
+
+Programul scris are rolul de a controla un Servo-motor, folosind platforma Blynk.
+
+## Exemplu Dashboard blynk
+![imagine blynk](https://i.imgur.com/KZD7TXB.png)
+
+## Detalii tehnice
+
+### ESP32
+- CPU: Xtensa dual-core
+- Performanta: 160 or 240 MHz si pana la 600 DMIPS
+- Memorie: 520 KiB SRAM
+- Bluetooth: V4.2 BR/EDR & BLE
+- Wi-Fi: 802.11 b/g/n
+- Power: 5uA deep sleep
+
+### DHT11
+- Tensiune alimentare: 3.3 - 5V
+- Cuplu: 2.5kg/cm
+- Rotatie: 0° - 180°
+
+## Link resurse
+- [BlynkLib.py](https://github.com/vshymanskyy/blynk-library-python/tree/master)
+- Platforma [Blynk.io](https://blynk.io/)
+- [Servo](https://www.xab3.ro/produse/servo-motor-9g), [ESP32](https://www.xab3.ro/produse/esp32-devkit-wh)
